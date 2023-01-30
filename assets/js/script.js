18ce6bb256ea12c594979d1eb72ea69aa13dd323
unknown <damianfearon@hotmail.com>
2023-01-30 21:21:47 +0000

    add comments for js file

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// 1. Declare a variable for city list
+// 2. Implement function to retrieve local storage history
+// 3. Store up to 8 cities in local storage as a list
+// 4. Add history button below search field using append
+// 5. Use moment.js to generate current date
+// 6. Retrieve icon code from API and assign to a variable
+// 7. Build main card with weather icon
+// 8. Combine city name, date, and moment format into a single variable
+// 9. Convert temperature from Kelvin and remove decimal places
+// 10. Create a variable to store the next request for UV index from API response
+// 11. Make individual API request for UV index and store response in a variable
+// 12. Send separate API request for UV index, using latitude and longitude
+// 13. Display UV index in main card
+// 14. Implement function for 5-day forecast API call
+// 15. Display 5 columns from forecast API response
+// 16. Show humidity in the dashboard
