daa4ae8cd4742339fe563a05dfb5ff975982de3b
unknown <damianfearon@hotmail.com>
2023-01-30 21:33:52 +0000

    arrange comment cards, add JSON for search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,52 @@
-// 1. Declare a variable for city list
-// 2. Implement function to retrieve local storage history
-// 3. Store up to 8 cities in local storage as a list
-// 4. Add history button below search field using append
-// 5. Use moment.js to generate current date
-// 6. Retrieve icon code from API and assign to a variable
-// 7. Build main card with weather icon
-// 8. Combine city name, date, and moment format into a single variable
-// 9. Convert temperature from Kelvin and remove decimal places
-// 10. Create a variable to store the next request for UV index from API response
-// 11. Make individual API request for UV index and store response in a variable
-// 12. Send separate API request for UV index, using latitude and longitude
-// 13. Display UV index in main card
-// 14. Implement function for 5-day forecast API call
-// 15. Display 5 columns from forecast API response
-// 16. Show humidity in the dashboard
+//  Declare a variable for city list
+var searchHistory = [];
+
+//  Implement function to retrieve local storage history
+function getItems() {
+    var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
+    if (storedCities !== null) {
+        searchHistory = storedCities;
+    };
+// Store up to 8 cities in local storage as a list
+
+// Add history button below search field using append
+
+
+// invokes getItems
+
+
+// main card
+
+
+    // requests
+  
+
+//  Use moment.js to generate current date
+
+
+//  Retrieve icon code from API and assign to a variable
+
+
+//  Build main card with weather icon
+
+//  Combine city name, date, and moment format into a single variable
+
+// Display name of city in main card
+
+// Display icon on the main card
+
+// Convert temperature from Kelvin and remove decimal places
+
+// Create a variable to store the next request for UV index from API response
+
+// Make individual API request for UV index and store response in a variable
+
+//  Send separate API request for UV index, using latitude and longitude
+
+// Display UV index in main card
+
+// Implement function for 5-day forecast API call
+
+//  Display 5 columns from forecast API response
+
+//  Show humidity in the dashboard
